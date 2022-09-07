e930b0d80ff5fe38cd87510809d6f3853d631afd
tinydom <tinydomsword@gmail.com>
2022-09-07 17:02:58 -0400

    Hero Section

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -66,11 +66,7 @@ const Hero = () => {
       <p>
         I’m a software engineer specializing in building (and occasionally designing) exceptional
         digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
-        </a>
-        .
+        in Blockchain industry.
       </p>
     </>
   );
